41a7fe150b7e47f5093ebb6d355f72ea3068c797
Aryan Sethi <sethiaryan2001@gmail.com>
2024-05-08 16:01:39 +1000

    wtw

--- MathHeaders/Vector4.cpp
+++ MathHeaders/Vector4.cpp
@@ -1,1 +1,23 @@
 #include "Vector4.h"
+
+MathClasses::Vector4::Vector4()
+{
+	x = 0;
+	y = 0;
+	z = 0;
+	w = 0;
+}
+
+MathClasses::Vector4::Vector4(float x, float y, float z, float w)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+	this->w = w;
+}
+
+std::string MathClasses::Vector4::ToString() const
+{
+	std::string vec4ToStr;
+	vec4ToStr = x + ' ' + y + ' ' + z + ' ' + w;
+}
